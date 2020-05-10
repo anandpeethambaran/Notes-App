de6329e581aa92fa2aa9f636c17be28013647b18
anandtp00 <anandtp00@gmail.com>
2020-05-10 22:40:48 +0530

    validator package from npm

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const fs=require('fs');
 
+const validator=require('validator');
+
+
 const util=require('./util');
 
 
@@ -9,5 +12,8 @@ const util=require('./util');
 
 
 // var outp=fs.readFileSync('notes.txt');
+
+const email="anand";
+console.log(validator.isEmail(email));
 
 console.log(util.getnotes());
