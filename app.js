d24cfb8f3263cbc844b157927b1d5d801d5d28af
anandtp00 <anandtp00@gmail.com>
2020-05-10 23:09:48 +0530

    command line arguments

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-const fs=require('fs');
-
 const validator=require('validator');
 const chalk=require('chalk');
 
 
-const util=require('./util');
 
 
-// fs.writeFileSync('notes.txt','The first file');
 
-// fs.appendFileSync('notes.txt',' Appending extra content');
-
-
-// var outp=fs.readFileSync('notes.txt');
-
-const email="anand";
-console.log(validator.isEmail(email));
-
-console.log(chalk.bold.blue("Success"));
-
-console.log(util.getnotes());
