87a347c2dfa4b62d6fdb5591c19f35e43d039c1b
anandtp00 <anandtp00@gmail.com>
2020-05-10 23:15:50 +0530

    using command line arguments

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const validator=require('validator');
-const chalk=require('chalk');
+const validator = require('validator');
+const chalk = require('chalk');
+
+const command = process.argv[2];
 
 
+console.log(process.argv);
+
+if (command === "add") {
+    console.log("This is to add a new note");
+}
+else if(command === "remove"){
+    console.log("This is to remove a new note");
+}
+else {
+    console.log("Another option")
+}
 
 
 
