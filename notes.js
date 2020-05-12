31d766fcaeeccc418ad65d73b0f02e12e1263330
anandtp00 <anandtp00@gmail.com>
2020-05-13 00:04:56 +0530

    adding new notes in json file

--- notes.js
+++ notes.js
@@ -6,18 +6,34 @@ const getNotes = () => {
     try{
         const notesData = fs.readFileSync('notes.json')
         const notesJSON = notesData.toString();
-        return JSON.stringify(notesJSON);
+        return JSON.parse(notesJSON);
 
     }catch(e){
         return []
     }
-  
 }
+
+const saveNotes = (notes)=>{
+    const dataJSON=JSON.stringify(notes);
+    fs.writeFileSync('notes.json',dataJSON);
+}
+
 
 module.exports = {
 
     addNotes: (title, body, author) => {
         const allNotes = getNotes()
+
+        allNotes.push({
+            title:title,
+            body:body,
+            author:author
+        })
+
+
+        saveNotes(allNotes)
+
+
         console.log(allNotes);
     },
     removeNotes: () => {
