d72062c92746cdc4bd2c8fc55ced53ef36363160
anandtp00 <anandtp00@gmail.com>
2020-05-12 23:09:20 +0530

    new created commands working after parsing

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+//notes app using command line argument
+
+//using yargs we are creating different options as commands
 
 
-console.log(process.argv);
 
-if (command === "add") {
-    console.log("This is to add a new note");
-}
-else if(command === "remove"){
-    console.log("This is to remove a new note");
-}
-else {
-    console.log("Another option")
-}
+//creating add command
+yargs.command({
+    command:'add',
+    describe:'Add a new Note',
+    handler: ()=>{
+        console.log('Adding a new note')
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler:()=>{
+        console.log('removing a note')
+    }
+})
+
+yargs.parse();
+
+
+// if (command === "add") {
+//     console.log("This is to add a new note");
+// }
+// else if (command === "remove") {
+//     console.log("This is to remove a new note");
+// }
+// else {
+//     console.log("Another option")
+// }
 
 
 
